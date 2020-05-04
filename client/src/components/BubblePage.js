227f9798e261170dcb9717b5f62df3b227603aa9
David Horstman <davidzilla27@gmail.com>
2020-05-03 21:46:00 -0400

    Edit and delete colors

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,17 +1,32 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
+
+import axiosWithAuth from "../api/axiosWithAuth";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
 const BubblePage = () => {
-  const [colorList, setColorList] = useState([]);
-  // fetch your colors data from the server when the component mounts
-  // set that data to the colorList state property
+  const [colorList, setColorList] = React.useState([]);
+  const [edits, setEdits] = React.useState(0);
+
+  React.useEffect(() => {
+    axiosWithAuth()
+      .get("/colors")
+      .then(r => setColorList(r.data))
+      .catch(console.error);
+  }, [edits]);
+
+  function triggerUpdate() {
+    setEdits(edits + 1);
+  }
 
   return (
     <>
-      <ColorList colors={colorList} updateColors={setColorList} />
+      <ColorList
+        colors={colorList}
+        updateColors={setColorList}
+        triggerUpdate={triggerUpdate}
+      />
       <Bubbles colors={colorList} />
     </>
   );
--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
-import axios from "axios";
+
+import axiosWithAuth from "../api/axiosWithAuth";
 
 const initialColor = {
   color: "",
-  code: { hex: "" }
+  code: { hex: "" },
 };
 
-const ColorList = ({ colors, updateColors }) => {
-  console.log(colors);
+const ColorList = ({ colors, updateColors, triggerUpdate }) => {
+  //console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
 
@@ -18,33 +19,41 @@ const ColorList = ({ colors, updateColors }) => {
 
   const saveEdit = e => {
     e.preventDefault();
-    // Make a put request to save your updated color
-    // think about where will you get the id from...
-    // where is is saved right now?
+    axiosWithAuth()
+      .put(`/colors/${colorToEdit.id}`, colorToEdit)
+      .then(console.log)
+      .then(triggerUpdate)
+      .then(() => setEditing(false))
+      .catch(console.error);
   };
 
-  const deleteColor = color => {
-    // make a delete request to delete this color
+  const deleteColor = ({ id }) => {
+    axiosWithAuth()
+      .delete(`/colors/${id}`)
+      .then(triggerUpdate)
+      .catch(console.error);
   };
 
   return (
-    <div className="colors-wrap">
+    <div className='colors-wrap'>
       <p>colors</p>
       <ul>
         {colors.map(color => (
           <li key={color.color} onClick={() => editColor(color)}>
             <span>
-              <span className="delete" onClick={e => {
-                    e.stopPropagation();
-                    deleteColor(color)
-                  }
-                }>
-                  x
+              <span
+                className='delete'
+                onClick={e => {
+                  e.stopPropagation();
+                  deleteColor(color);
+                }}
+              >
+                x
               </span>{" "}
               {color.color}
             </span>
             <div
-              className="color-box"
+              className='color-box'
               style={{ backgroundColor: color.code.hex }}
             />
           </li>
@@ -68,19 +77,19 @@ const ColorList = ({ colors, updateColors }) => {
               onChange={e =>
                 setColorToEdit({
                   ...colorToEdit,
-                  code: { hex: e.target.value }
+                  code: { hex: e.target.value },
                 })
               }
               value={colorToEdit.code.hex}
             />
           </label>
-          <div className="button-row">
-            <button type="submit">save</button>
+          <div className='button-row'>
+            <button type='submit'>save</button>
             <button onClick={() => setEditing(false)}>cancel</button>
           </div>
         </form>
       )}
-      <div className="spacer" />
+      <div className='spacer' />
       {/* stretch - build another form here to add a color */}
     </div>
   );

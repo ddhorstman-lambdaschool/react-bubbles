90207ff476ef50e823d61ab4de62b5a3eda53ac7
David Horstman <davidzilla27@gmail.com>
2020-05-03 21:27:55 -0400

    Login

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,30 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import useLocalStorage from "./hooks/useLocalStorage";
+
 import Login from "./components/Login";
+import PrivateRoute from "./components/PrivateRoute";
+import BubblePage from "./components/BubblePage";
 import "./styles.scss";
 
-function App() {
+export default function App() {
+  const [token, setToken] = useLocalStorage("token");
   return (
     <Router>
-      <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+      <div className='App'>
+        <Route
+          exact
+          path='/'
+          render={params => <Login {...params} setToken={setToken} />}
+        />
+        <PrivateRoute
+          exact
+          path='/bubbles'
+          component={BubblePage}
+          redirectPath={"/"}
+        />
       </div>
     </Router>
   );
 }
-
-export default App;
---
+++ client/src/api/axiosWithAuth.js
@@ -0,0 +1,90 @@
+import axios from "axios";
+
+const baseURL = "http://localhost:5000/api";
+
+/**
+ * A function to perform axios calls which automatically include
+ * an authorization token specified by the "token" key in localStorage.
+ * @param {object} [options] The options to be passed to axios.create()
+ */
+export function axiosWithAuth(options) {
+  let token = localStorage.getItem("token");
+  if (token === null) {
+    throw new Error(
+      `No token found. axiosWithAuth requires a token to be placed in localStorage under the "token" key.`
+    );
+  }
+  try {
+    //parse a JSON-encoded token, such as one created by useLocalStorage
+    token = JSON.parse(token);
+    //console.log(`Found stringified token in localStorage: ${token}`);
+  } catch {
+    //console.log(`Found non-stringified token in localStorage: ${token}`);
+  }
+  return axios.create({
+    baseURL,
+    ...options,
+    headers: {
+      ...options?.headers,
+      Authorization: token,
+    },
+  });
+}
+
+/**
+ * A function to perform axios calls similarly to axiosWithAuth,
+ * but without automatically including an authorization token.
+ * @param {object} [options] The options to be passed to axios.create()
+ */
+export function axiosWithoutAuth(options) {
+  return axios.create({
+    baseURL,
+    ...options,
+  });
+}
+
+/**
+ * Create an axios instance which automatically includes
+ * an authorization token specified by the "token" key in
+ * localStorage as well as the ability to cancel the call
+ * if the component unmounts.
+ * @param {object} [options] The options to pass on to axios.create()
+ */
+export function axiosWithAuthCancellable(options) {
+  let source = axios.CancelToken.source();
+  let unmountedInternal = false;
+
+  /**
+   * @returns {boolean} Whether the component has been unmounted
+   */
+  const unmounted = () => unmountedInternal;
+
+  /**
+   * A function to cancel the API call in progress and
+   * prevent any downstream changes based on the unmounted() property.
+   * Call in componentWillUnmount() or return from useEffect().
+   */
+  const cancelAPICall = () => {
+    unmountedInternal = true;
+    source.cancel("Component unmounted. Data fetching cancelled.");
+  };
+
+  /**
+   * A function to perform axios calls which automatically include
+   * an authorization token specified by the "token" key in
+   * localStorage as well as the ability to cancel the call
+   * if the component unmounts using cancelAPICall().
+   * @param {object} [optionsInner] The options to be passed to axios.create()
+   */
+  const axiosWithAuthC = (optionsInner = options) =>
+    axiosWithAuth({ ...optionsInner, cancelToken: source.token });
+
+  return {
+    isCancel: axios.isCancel,
+    unmounted,
+    axiosWithAuth: axiosWithAuthC,
+    cancelAPICall,
+  };
+}
+
+export default axiosWithAuth;
